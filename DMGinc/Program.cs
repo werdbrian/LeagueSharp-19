45c4d9752d03d3b569a1d502b45737c9d1d3ebb2
Jaxy <cryzar95@googlemail.com>
2015-02-14 16:53:08 +0100

    Change

---
+++ DMGinc/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using LeagueSharp;
+
+
+namespace DMGinc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game.PrintChat("Hey");
+        }
+    }
+}

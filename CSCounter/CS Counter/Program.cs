acd615f1fdbc5baed3854f7eea941c22aeae4a3e
Kyon <cryzar95@googlemail.com>
2015-05-05 21:22:02 +0200

    XD

--- CSCounter/CS Counter/Program.cs
+++ CSCounter/CS Counter/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Drawing;
+using CS_Counter.Properties;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
+using SharpDX.Direct3D9;
+using Font = SharpDX.Direct3D9.Font;
 
 namespace CS_Counter
 {
@@ -33,15 +37,21 @@ namespace CS_Counter
         public static int Wavethree;
         private static int _percent;
 
+        public static Texture CdFrameTexture;
+        public static Sprite Sprite;
+        public static Font Textx;
+        public static int X;
+        public static int Y;
+
         public static bool Enabled = true;
 
         static void Main(string[] args)
         {
             if (args == null) throw new ArgumentNullException("args");
-            Game_OnGameLoad();
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
         }
 
-        private static void Game_OnGameLoad()
+        private static void Game_OnGameLoad(EventArgs args)
         {
 
             Notifications.AddNotification("CS Counter loaded.", 10);
@@ -50,6 +60,20 @@ namespace CS_Counter
             //var drawings2 = new Menu("Drawings", "drawings2");
             //_menu2.AddSubMenu(drawings2);
 
+            Sprite = new Sprite(Drawing.Direct3DDevice);
+            CdFrameTexture = Texture.FromMemory(
+                    Drawing.Direct3DDevice, (byte[])new ImageConverter().ConvertTo(Resources.Untitled_34, typeof(byte[])), 147,
+                    27, 0, Usage.None, Format.A1, Pool.Managed, Filter.Default, Filter.Default, 0);
+
+            Textx = new Font(
+                    Drawing.Direct3DDevice,
+                    new FontDescription
+                    {
+                        FaceName = "Calibri",
+                        Height = 13,
+                        OutputPrecision = FontPrecision.Default,
+                        Quality = FontQuality.Default
+                    });
 
             _menuenable2 = new MenuItem("menu.drawings.enable2", "CS Count").SetValue(true);
             _menu2.AddItem(_menuenable2);
@@ -85,6 +109,8 @@ namespace CS_Counter
             var x = new TimeSpan(0, 0, 0, 18);
             var var = TimeSpan.FromSeconds(Game.Time) - x;
 
+           
+
             if (!(var <= _minionspawn))
             {
                 var wave = (((int)var.TotalSeconds - (int)_minionspawn.TotalSeconds) / (int)_timeplus.TotalSeconds) + 1;
@@ -94,10 +120,13 @@ namespace CS_Counter
 
             GetCsEnemy();
 
+            
         }
 
         private static void GetCsEnemy()
         {
+            Sprite.Begin(SpriteFlags.AlphaBlend);
+
             foreach (var hero in ObjectManager.Get<Obj_AI_Hero>())
             {
 
@@ -137,6 +166,7 @@ namespace CS_Counter
                     }
 
                     Text.OnEndScene();
+                    Sprite.Draw(CdFrameTexture, new ColorBGRA(255, 255, 255, 255), null, new Vector3(-barPos.X + 25, -barPos.Y, 0));
                     continue;
                     
                 }
@@ -147,8 +177,12 @@ namespace CS_Counter
                     Text.text = "CS Count: " + cs;
                     Text.OnEndScene();
 
+                Sprite.Draw(CdFrameTexture, new ColorBGRA(255, 255, 255, 255), null, new Vector3(-barPos.X - 25, -barPos.Y - 6 , 0));
+
+                
             }
 
+            Sprite.End();
         }
     }
 }

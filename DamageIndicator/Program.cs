f26f63340c826b5250e102ce87d361af1e3aad49
werdbrian <werdbrian@gmail.com>
2015-08-25 01:08:33 -0700

    Update Program.cs

--- DamageIndicator/Program.cs
+++ DamageIndicator/Program.cs
@@ -124,7 +124,7 @@ namespace DamageIndicator
         private static float GetComboDamage(Obj_AI_Base enemy)
         {
             // do nothing is there no enemy
-            if (enemy == null) return;
+            if (enemy == null) return 0;
 
             // reset values
             double damage = 0d;
